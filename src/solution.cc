c3676231296a3b8673d279145522669735a6d18f
Tillu1618 <avik2781@gmail.com>
2024-09-15 20:02:12 +0000

    Fixed reading lines (removed place_holder_2, checked for commas)

--- src/solution.cc
+++ src/solution.cc
@@ -10,19 +10,20 @@ Student ReadStudentRecFromStream(std::istream& is) {
   std::string first_name("");
   std::string last_name("");
   char place_holder = '\0';
-  char place_holder_2 = '\0';
   unsigned int uin = 0;
   double gpa = 0;
 
   // Read the line, assign values to variables
-  is >> first_name >> last_name >> place_holder >> uin >> place_holder_2 >> gpa;
+  is >> first_name >> last_name >> uin >> place_holder >> gpa;
 
   // Check if input file stream failed
-  if (is.fail() || place_holder != ',' || place_holder_2 != ',')
+  if (is.fail() || place_holder != ',' ||
+      last_name[last_name.size() - 1] != ',')
     return Student{};
 
   // Combine first_name and last_name
-  std::string name = first_name + ' ' + last_name;
+  std::string name =
+      first_name + ' ' + last_name.substr(0, last_name.size() - 1);
 
   // Return student
   return Student{name, uin, gpa};
