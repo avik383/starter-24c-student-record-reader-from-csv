1cfa7540d955b530795a169a510a604a1ee5e5e8
Tillu1618 <avik2781@gmail.com>
2024-09-15 19:36:39 +0000

    Added place_holder_2 variable to ensure second comma

--- src/solution.cc
+++ src/solution.cc
@@ -10,14 +10,16 @@ Student ReadStudentRecFromStream(std::istream& is) {
   std::string first_name("");
   std::string last_name("");
   char place_holder = '\0';
+  char place_holder_2 = '\0';
   unsigned int uin = 0;
   double gpa = 0;
 
   // Read the line, assign values to variables
-  is >> first_name >> last_name >> place_holder >> uin >> place_holder >> gpa;
+  is >> first_name >> last_name >> place_holder >> uin >> place_holder_2 >> gpa;
 
   // Check if input file stream failed
-  if (is.fail()) return Student{};
+  if (is.fail() || place_holder != ',' || place_holder_2 != ',')
+    return Student{};
 
   // Combine first_name and last_name
   std::string name = first_name + ' ' + last_name;
